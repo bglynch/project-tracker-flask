9a81c84fb975641cfa29c888408f50d585653a36
Brian Lynch <bglynch17@gmail.com>
2018-08-20 14:01:15 +0000

    login form validation and error messages

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from .forms import LoginForm
 
 @app.route('/')
@@ -23,7 +23,10 @@ def index():
     return render_template('index.html', title='Home', user=user, projects=projects)
     
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))    
     return render_template('login.html', title='Sign In', form=form)
