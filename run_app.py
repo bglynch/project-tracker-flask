fc9a6edd399940e9c83ffc14b69ec1d7c6111ad2
Brian Lynch <bglynch17@gmail.com>
2018-08-20 10:51:07 +0000

    set up virtualenv: venv, created flask Hello World

---
+++ run_app.py
@@ -0,0 +1,8 @@
+from app import app
+import os
+
+if __name__ == '__main__':
+    app.debug = True
+    host = os.environ.get('IP', '0.0.0.0')
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host=host, port=port)
