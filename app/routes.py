0b2d855099082c66e3b933a225e39a814f0e2dae
Brian Lynch <bglynch17@gmail.com>
2018-08-20 11:19:48 +0000

    added basic templates and dummy data

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Brian'}
+    projects = [
+        {
+            'number': 134,
+            'name': 'Temple Villa Road',
+            'type':'Rear Extension',
+            'value': 400
+        },
+        {
+            'number': 185,
+            'name':   'Electric Ave.',
+            'type':   'Loft Conversion',
+            'value':  350
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, projects=projects)
