f0e228924598535b8a0de7b549d6ee24926730e5
Brian Lynch <bglynch17@gmail.com>
2018-08-21 19:30:48 +0000

    added user login

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id              = db.Column(db.Integer, primary_key=True)
     username        = db.Column(db.String(64), index=True, unique=True)
     email           = db.Column(db.String(120), index=True, unique=True)
@@ -18,6 +19,9 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Project(db.Model):
     id          = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from .forms import LoginForm
+from flask_login import current_user, login_user
+from .models import User
 
 @app.route('/')
 @app.route('/index')
@@ -26,7 +28,13 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))    
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))    
     return render_template('login.html', title='Sign In', form=form)
