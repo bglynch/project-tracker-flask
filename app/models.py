cb21fc5b1330f9c12e35b1bf24df9cce8769de64
Brian Lynch <bglynch17@gmail.com>
2018-08-21 18:52:50 +0000

    password methods added to User

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id              = db.Column(db.Integer, primary_key=True)
@@ -9,7 +10,13 @@ class User(db.Model):
     projects        = db.relationship('Project', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username) 
+        return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Project(db.Model):
