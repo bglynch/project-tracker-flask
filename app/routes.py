5f17f82f22b29ac4eba8ba9e1db5aef35b08576d
Brian Lynch <bglynch17@gmail.com>
2018-08-22 09:58:52 +0000

    require user login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from .forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from .models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Brian'}
     projects = [
@@ -36,7 +38,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))    
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)        
     return render_template('login.html', title='Sign In', form=form)
     
 @app.route('/logout')
