25b1c24ab83de7bfbf92996693df133acbd75425
Brian Lynch <bglynch17@gmail.com>
2018-08-22 09:53:34 +0000

    add user logout

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from .forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from .models import User
 
 @app.route('/')
@@ -27,9 +27,9 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
     if current_user.is_authenticated:
         return redirect(url_for('index'))    
+    form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
@@ -38,3 +38,10 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))    
     return render_template('login.html', title='Sign In', form=form)
+    
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))    
+
+
