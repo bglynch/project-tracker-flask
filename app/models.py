eeb4d49a0735cd8aded1367983b918832389982b
Brian Lynch <bglynch17@gmail.com>
2018-09-06 14:39:43 +0000

    task model created

--- app/models.py
+++ app/models.py
@@ -33,8 +33,18 @@ class Project(db.Model):
     completed   = db.Column(db.Boolean, default=False, nullable=False)
     client      = db.Column(db.String(140))
     user_id     = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    tasks       = db.relationship('Task', backref='task', lazy=True)
 
     def __repr__(self):
         return '<Project {}>'.format(self.name)
 
-    
+class Task(db.Model):
+    id           = db.Column(db.Integer, primary_key=True)
+    title        = db.Column(db.String(140))
+    description  = db.Column(db.Integer, nullable=False)
+    status       = db.Column(db.Boolean, default=False, nullable=False)
+    project_id   = db.Column(db.Integer, db.ForeignKey('project.id'),nullable=False)
+
+    def __repr__(self):
+        return '<Task {}>'.format(self.name)
+
