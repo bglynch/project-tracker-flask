c9b578a042032c7681e7d9bab25f5aa1ef578b31
Brian Lynch <bglynch17@gmail.com>
2018-10-03 14:50:22 +0000

    functionality: added email recovery using a gmail account

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request, jsonify
-from .forms import LoginForm, RegistrationForm, ProjectForm, TaskForm, TaskCompleteForm
+from .forms import LoginForm, RegistrationForm, ProjectForm, TaskForm, TaskCompleteForm, ResetPasswordRequestForm, ResetPasswordForm
 from flask_login import current_user, login_user, logout_user, login_required
 from .models import User, Project, Task
 from werkzeug.urls import url_parse
 from sqlalchemy.orm import lazyload
+from app.email import send_password_reset_email
 
 # ------------------------------------------------------- HOME
 @app.route('/')
@@ -98,7 +99,7 @@ def view_project(username, projectno):
     tasks = Task.query.filter_by(project_id=projectno)
     job = Project.query.filter_by(id=projectno).first_or_404()
     print(len(tasks[0:]))
-    return render_template('projects_tasks.html', tasks=tasks, job=job, form=form)
+    return render_template('projects_page.html', tasks=tasks, job=job, form=form)
     
 
 @app.route('/<username>/<projectno>/add_task', methods=['GET', 'POST'])
@@ -189,3 +190,31 @@ def user_data(username):
     
     return jsonify(data)
     
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('forms/reset_password_request.html',
+                           title='Reset Password', form=form)
+                           
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('forms/reset_password.html', form=form)
