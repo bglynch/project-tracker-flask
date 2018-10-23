fb4f3f89ffaddeef2930fed952089d8c982a781c
Brian Lynch <bglynch17@gmail.com>
2018-10-23 19:12:28 +0000

    updated readme, set debug to false

--- run_app.py
+++ run_app.py
@@ -2,7 +2,7 @@ import os
 from app import app
 
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = False
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 8080))
     app.run(host=host, port=port)
